695b9dc32af27eb9038d3af653158688c0c8e079
tim_graves <28924492+atimgraves@users.noreply.github.com>
2023-08-09 21:35:38 +0100

    update test code to operate on a loop, add support for optionally trimming input of white space. Add copyright to meet oracle GH rules

--- functions-hmac/src/main/java/com/oracle/timg/hmac/test/GenerateTestHMAC.java
+++ functions-hmac/src/main/java/com/oracle/timg/hmac/test/GenerateTestHMAC.java
@@ -1,3 +1,39 @@
+/*Copyright (c) 2023 Oracle and/or its affiliates.
+
+The Universal Permissive License (UPL), Version 1.0
+
+Subject to the condition set forth below, permission is hereby granted to any
+person obtaining a copy of this software, associated documentation and/or data
+(collectively the "Software"), free of charge and under any and all copyright
+rights in the Software, and any and all patent rights owned or freely
+licensable by each licensor hereunder covering either (i) the unmodified
+Software as contributed to or provided by such licensor, or (ii) the Larger
+Works (as defined below), to deal in both
+
+(a) the Software, and
+(b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+one is included with the Software (each a "Larger Work" to which the Software
+is contributed by such licensors),
+
+without restriction, including without limitation the rights to copy, create
+derivative works of, display, perform, and distribute the Software and make,
+use, sell, offer for sale, import, export, have made, and have sold the
+Software and the Larger Work(s), and to sublicense the foregoing rights on
+either these or other terms.
+
+This license is subject to the following condition:
+The above copyright notice and either this complete permission notice or at
+a minimum a reference to the UPL must be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 package com.oracle.timg.hmac.test;
 
 import java.io.File;
@@ -23,6 +59,7 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.http.HttpStatus;
 
 import com.oracle.timg.hmac.VerifyHmacFunction;
 
@@ -41,10 +78,14 @@ public class GenerateTestHMAC {
 	private static Option urlOption;
 	private static Option hmacHeaderOption;
 	private static Option httpRequestOption;
+	private static Option countOption;
+	private static Option delayOption;
+	private static Option exitOption;
+	private static Option trimOption;
 
 	public static void main(String[] args) throws IOException {
 		// build the input string, the options are
-		// -h / --header <name>:<value>[,<name:value>] one of more entries the name must
+		// -h / --header <name>=<value>[,<name=value>] one of more entries the name must
 		// be a HTTP
 		// header format in lower case with - not _
 		// -b / --body <file> defaults to body.txt
@@ -61,50 +102,76 @@ public class GenerateTestHMAC {
 		// made
 		// empty string)
 		// these are
-		VerifyHmacFunction vhm = new VerifyHmacFunction();
 		CommandLine commandLine = getCommandLine(args);
-		Map<String, String> headers = getDataOptions(commandLine, args, vhm);
+		VerifyHmacFunction vhm = new VerifyHmacFunction();
+		Map<String, String> headers = getDataOptions(commandLine, vhm);
 		Map<String, String> fields = new HashMap<>(headers);
 		// the fields map is used in the HMAC calculation, so add any required fields to
 		// that
 		addHMACCalculationEntries(commandLine, fields);
 		log.info("Retrieved the following fields\n" + fields);
-		String calculatedHmac = vhm.processRequest(fields);
+		String calculatedHmac;
+		try {
+			calculatedHmac = vhm.processRequest(fields);
+		} catch (Exception e) {
+			log.error("Exception generating calculated hmac " + e.getLocalizedMessage());
+			return;
+		}
 		log.info("Calculated HMAC is " + calculatedHmac);
 		if (commandLine.hasOption(urlOption)) {
 			// this must have a value to have got here
 			String url = commandLine.getOptionValue(urlOption);
 			String hmacHeader = commandLine.getOptionValue(hmacHeaderOption, "hmac");
+			int count = Integer.parseInt(commandLine.getOptionValue(countOption, "1"));
+			int delay = Integer.parseInt(commandLine.getOptionValue(delayOption, "10"));
+			boolean exitOnFail = commandLine.hasOption(exitOption);
 			headers.put(hmacHeader, calculatedHmac);
 			String requestMethod = commandLine.getOptionValue(httpRequestOption, "POST");
-			callUrl(requestMethod, url, headers, readFile(commandLine.getOptionValue(bodyOption, "body.txt")));
+			callUrl(requestMethod, url, headers, readFile(commandLine.getOptionValue(bodyOption, "body.txt")), count,
+					delay, exitOnFail);
 		}
 	}
 
-	private static void callUrl(String requestMethod, String url, Map<String, String> headers, String body) {
+	private static void callUrl(String requestMethod, String url, Map<String, String> headers, String body, int count,
+			int delay, boolean exitOnFail) {
 		log.info("Making " + requestMethod + "\nCalling " + url + "\nWith headers" + headers + "\nrequest body is\n"
 				+ body);
-		HttpClient client = HttpClient.newHttpClient();
-		URI uri;
-		try {
-			uri = new URI(url);
-		} catch (URISyntaxException e) {
-			log.error("Provided URL " + url + " does not parse properly because " + e.getLocalizedMessage());
-			return;
+		for (int i = 1; i <= count; i++) {
+			HttpClient client = HttpClient.newHttpClient();
+			URI uri;
+			try {
+				uri = new URI(url);
+			} catch (URISyntaxException e) {
+				log.error("Provided URL " + url + " does not parse properly because " + e.getLocalizedMessage());
+				return;
+			}
+			HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
+					.method(requestMethod, BodyPublishers.ofString(body)).uri(uri);
+			headers.entrySet().stream().forEach(entry -> requestBuilder.header(entry.getKey(), entry.getValue()));
+			HttpRequest request = requestBuilder.build();
+			HttpResponse<String> response;
+			try {
+				response = client.send(request, BodyHandlers.ofString());
+			} catch (IOException | InterruptedException e) {
+				log.error("Exception making the http call:" + e.getLocalizedMessage());
+				return;
+			}
+			log.info("Try " + i + " Response status code is " + response.statusCode());
+			log.info("Try " + i + " Response body is:\n" + response.body());
+			if (exitOnFail && response.statusCode() != HttpStatus.SC_OK) {
+				log.info("Non OK response, stopping");
+				return;
+			}
+			if (i < count) {
+				try {
+					log.info("Completed try " + i + " waitign " + delay + " seconds ");
+					Thread.sleep((long) (1000 * delay));
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
-		HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
-				.method(requestMethod, BodyPublishers.ofString(body)).uri(uri);
-		headers.entrySet().stream().forEach(entry -> requestBuilder.header(entry.getKey(), entry.getValue()));
-		HttpRequest request = requestBuilder.build();
-		HttpResponse<String> response;
-		try {
-			response = client.send(request, BodyHandlers.ofString());
-		} catch (IOException | InterruptedException e) {
-			log.error("Exception making the http call:" + e.getLocalizedMessage());
-			return;
-		}
-		log.info("Response status code is " + response.statusCode());
-		log.info("Responde body is:\n" + response.body());
 	}
 
 	private static CommandLine getCommandLine(String args[]) {
@@ -129,7 +196,7 @@ public class GenerateTestHMAC {
 		fields.put("SALT", commandLine.getOptionValue(saltOption, ""));
 	}
 
-	private static Map<String, String> getDataOptions(CommandLine commandLine, String args[], VerifyHmacFunction vhm) {
+	private static Map<String, String> getDataOptions(CommandLine commandLine, VerifyHmacFunction vhm) {
 		// the fields to process
 		Map<String, String> fields = new HashMap<>();
 		// extract the headers
@@ -138,7 +205,10 @@ public class GenerateTestHMAC {
 			headers = new String[0];
 		}
 		for (String header : headers) {
-			String headerEntry[] = header.split(":");
+			String headerEntry[] = header.split("=");
+			if (headerEntry.length < 2) {
+				log.error("Header " + headerEntry + " is missing a value");
+			}
 			fields.put(VerifyHmacFunction.mungeHeader(headerEntry[0]), headerEntry[1]);
 		}
 
@@ -148,7 +218,8 @@ public class GenerateTestHMAC {
 		VerifyHmacFunction.HMAC_SECRET = commandLine.getOptionValue(keyOption, "Secret");
 		VerifyHmacFunction.HMAC_ALGORITHM = commandLine.getOptionValue(algorithmOption, "HmacMD5");
 		VerifyHmacFunction.HMAC_HMAC_CALCULATION_FIELD_SEPARATOR = commandLine.getOptionValue(separatorOption, "");
-
+		VerifyHmacFunction.HMAC_INPUT_FIELDS_TRIM = Boolean
+				.parseBoolean(commandLine.getOptionValue(trimOption, "true"));
 		return fields;
 	}
 
@@ -162,7 +233,8 @@ public class GenerateTestHMAC {
 	}
 
 	private static Options setupCommandLineOptions() {
-		headerOption = Option.builder("h").longOpt("header").optionalArg(true).hasArg().desc("A header to add").build();
+		headerOption = Option.builder("h").longOpt("header").optionalArg(true).hasArg().valueSeparator(',')
+				.desc("A header to add").build();
 		bodyOption = Option.builder("b").longOpt("body").optionalArg(true).hasArg()
 				.desc("The file containign the text of the request body. defaults to body.txt").build();
 		saltOption = Option.builder("s").longOpt("salt").optionalArg(true).hasArg()
@@ -181,9 +253,19 @@ public class GenerateTestHMAC {
 				.desc("Header to place the calculoated HMAC in when making a test request, defaults to hmac").build();
 		httpRequestOption = Option.builder("r").longOpt("httprequest").optionalArg(true).hasArg()
 				.desc("HTTP request method to call, defaults to POST").build();
+		countOption = Option.builder("c").longOpt("count").optionalArg(true).hasArg()
+				.desc("Number of times to make the request to the backend service, defaults to 1").build();
+		delayOption = Option.builder("d").longOpt("delay").optionalArg(true).hasArg()
+				.desc("Seconds delay between calls to the backend service, defaults to 10").build();
+		exitOption = Option.builder("x").longOpt("exit")
+				.desc("If present will stop processing if a call does not return sucess").build();
+		trimOption = Option.builder("t").longOpt("trim").optionalArg(true).hasArg().desc(
+				"If true will remove whitespace from the input fields when combining to calculate the HMAC, if false any whitespace will be included. Defaults to true")
+				.build();
 		Options options = new Options().addOption(headerOption).addOption(bodyOption).addOption(saltOption)
 				.addOption(fieldsOption).addOption(keyOption).addOption(algorithmOption).addOption(separatorOption)
-				.addOption(urlOption).addOption(hmacHeaderOption).addOption(httpRequestOption);
+				.addOption(urlOption).addOption(hmacHeaderOption).addOption(httpRequestOption).addOption(countOption)
+				.addOption(delayOption).addOption(exitOption).addOption(trimOption);
 		return options;
 	}
 }
