65e87ae4463dc1c732f22151fdf56fa6deb2028b
tim_graves <28924492+atimgraves@users.noreply.github.com>
2026-06-17 14:30:56 +0100

    somehow the incoming data package got deleted, this restores it

---
+++ IoTSonnenUploader/src/main/java/com/oracle/demo/timg/iot/iotsonnenuploader/incommingdata/SonnenConfiguration.java
@@ -0,0 +1,80 @@
+/*Copyright (c) 2025 Oracle and/or its affiliates.
+
+The Universal Permissive License (UPL), Version 1.0
+
+Subject to the condition set forth below, permission is hereby granted to any
+person obtaining a copy of this software, associated documentation and/or data
+(collectively the "Software"), free of charge and under any and all copyright
+rights in the Software, and any and all patent rights owned or freely
+licensable by each licensor hereunder covering either (i) the unmodified
+Software as contributed to or provided by such licensor, or (ii) the Larger
+Works (as defined below), to deal in both
+
+(a) the Software, and
+(b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+one is included with the Software (each a "Larger Work" to which the Software
+is contributed by such licensors),
+
+without restriction, including without limitation the rights to copy, create
+derivative works of, display, perform, and distribute the Software and make,
+use, sell, offer for sale, import, export, have made, and have sold the
+Software and the Larger Work(s), and to sublicense the foregoing rights on
+either these or other terms.
+
+This license is subject to the following condition:
+The above copyright notice and either this complete permission notice or at
+a minimum a reference to the UPL must be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+package com.oracle.demo.timg.iot.iotsonnenuploader.incommingdata;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.micronaut.serde.annotation.Serdeable;
+import lombok.Data;
+
+@Serdeable
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class SonnenConfiguration {
+	// get the UTC TZ once to speed things later
+	@JsonIgnore
+	private final static ZoneId utcTz = ZoneId.of("UTC");
+	public static String PLACE_HOLDER_VALUE = "CommandTestPlaceholder";
+	// store this in two formats as the IoT service uses the Unix time, but we might
+	// want to process it based on time zone data, however force the zone to be GMT
+	@JsonFormat(pattern = "uuuu-MM-dd'T'HH:mm:ss.SSSXXX")
+	public ZonedDateTime timestamp = ZonedDateTime.now(utcTz);
+	public long time = System.currentTimeMillis();
+
+	@JsonProperty("EM_ToU_Schedule")
+	public void setTimeOfUseScheduleFromSonnen(String timeOfUseSchedule) {
+		this.timeOfUseSchedule = timeOfUseSchedule;
+	}
+
+	private String timeOfUseSchedule;
+
+	@JsonProperty("DE_Software")
+	public void setSoftwareVersionFromSonnen(String softwareVersion) {
+		this.softwareVersion = softwareVersion;
+	}
+
+	private String softwareVersion;
+
+	private String commandDemoPlaceholder = PLACE_HOLDER_VALUE;
+
+}
