7b7b76f14b92337afe17e1b9540c1de914e346ad
tim_graves <28924492+atimgraves@users.noreply.github.com>
2026-07-08 12:23:54 +0100

    rename the adapter files to Routes as it's a better description

--- IoTSonnenUploader/GatewayProxy/gateway_names.sh
+++ IoTSonnenUploader/GatewayProxy/gateway_names.sh
@@ -1,7 +1,7 @@
 # these define the various names for the gateway objects
 export GATEWAY_MODEL_NAME=hagateway
 export GATEWAY_MODEL_FILE=file://GatewayDTMI.json
-export GATEWAY_ADAPTOR_ROUTE_MAPPINGS_FILE=file://GatewayProxyAdapter.json
+export GATEWAY_ADAPTOR_ROUTE_MAPPINGS_FILE=file://GatewayProxyRoutes.json
 export GATEWAY_ADAPTOR_ENVELOPE_MAPPINGS_FILE=file://GatewayProxyEnvelope.json
 export GATEWAY_ADAPTER_NAME=gateway-adapter
 # do not change these endpoint paths without also altering the Java code and the DTML route files in the adapter
